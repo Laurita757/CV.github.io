6e8b25c6696e06fa86bb3fe96d885f305002618e
María Laura Aoki <aokimarialaura@gmail.com>
2023-02-25 18:40:23 -0300

    Con el formulario listo

--- cv-interactivo.js
+++ cv-interactivo.js
@@ -1,10 +1,5 @@
 //Formulario de Contacto.
-document.getElementById("botonEnviar").addEventListener("click", function() {
-    console.log("Probando el formulario")
-    document.getElementById("botonEnviar").innerHTML = alert("La consulta ha sido enviada")
-});
-
-function myFunction() {
-    let text = document.getElementById("myInput").value;
-    document.getElementById("demo").innerHTML = "You wrote: " + text;
-  }
+function validateForm() {
+    alert('Consulta enviada');
+    console.log("probando el formulario");
+  }   
